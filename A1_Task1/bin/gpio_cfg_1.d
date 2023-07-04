a23d3562efb660821daf7da00a2bde04f1edf47c
Hossam elwahsh <hacker2894@gmail.com>
2023-07-04 03:01:45 +0300

    refactor: untrack bin output

--- A1_Task1/bin/gpio_cfg_1.d
+++
@@ -1,4 +0,0 @@
-.\bin\gpio_cfg_1.o: src\GPIO_cfg.c
-.\bin\gpio_cfg_1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\bin\gpio_cfg_1.o: .\headers\GPIO.h
-.\bin\gpio_cfg_1.o: .\headers\GPIO_cfg.h
--- A1_Task1/bin/portasm.d
+++
@@ -1,2 +0,0 @@
-.\bin\portasm.o: ..\FreeRTOS\Source\portable\RVDS\ARM7_LPC21xx\portASM.s
-.\bin\portasm.o: ..\FreeRTOS\Source\portable\RVDS\ARM7_LPC21xx\portmacro.inc
